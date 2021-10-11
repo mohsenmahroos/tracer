885e9ae4f6fee8728cefaf219ea2ac73c02163d5
mohsenmahroos <mohsenmahroos@msn.com>
2021-10-11 17:51:37 +0200

    Update tracer.hpp

--- tracer.hpp
+++ tracer.hpp
@@ -4,14 +4,14 @@
 #include <iostream>
 namespace tracer {
     inline void pause() { std::cin.get(); }
-    template<class Arg, class...Args>
+    template<class Arg, class... Args>
     inline void pause(const Arg& arg, const Args&... args) { std::cout << arg, pause(args...); }
     class tracer_t {
         int depth = 0;
         inline std::string prefix(int line) const {
             return std::string(depth,' ')+std::to_string(depth)+":LINE "+std::to_string(line)+": "; }
         inline void comma_separated() const {}
-        template<class Arg, class...Args>
+        template<class Arg, class... Args>
         inline void comma_separated(const Arg& arg, const Args&... args) const {
             if (std::is_same<Arg,std::string>::value)
                 std::cout << "\"" << arg << "\"";
@@ -19,18 +19,18 @@ namespace tracer {
                 std::cout << arg;
             if (sizeof...(Args) > 0)
                 std::cout << ",", comma_separated(args...); }
-        template<class...Args>
-        inline void header(int line, const std::string& fun, const Args&...args) const {
+        template<class... Args>
+        inline void header(int line, const std::string& fun, const Args&... args) const {
             std::cout << prefix(line) << fun << "(", comma_separated(args...), std::cout << ") "; }
     public:
-        template<class...Args>
-        inline void begin(int line, const std::string& fun, const Args&...args) {
+        template<class... Args>
+        inline void begin(int line, const std::string& fun, const Args&... args) {
             ++depth, header(line,fun,args...), pause("called\n"); }
-        template<class...Args>
-        inline void trace(int line, const std::string& fun, const Args&...args) {
+        template<class... Args>
+        inline void trace(int line, const std::string& fun, const Args&... args) {
             header(line,fun,args...), pause("\n"); }
-        template<class Ans, class...Args>
-        inline Ans end(int line, const std::string& fun, const Ans& ans, const Args&...args) {
+        template<class Ans, class... Args>
+        inline Ans end(int line, const std::string& fun, const Ans& ans, const Args&... args) {
             header(line,fun,args...), --depth, pause("return value = ",ans,"\n");
             return ans; } } object; }
 #define tr_begin(...) tracer::object.begin(__LINE__,__FUNCTION__,__VA_ARGS__)

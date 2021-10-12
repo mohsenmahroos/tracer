dcc572f6ee4bdd404260cd207491d36aeb47bf89
mohsenmahroos <mohsenmahroos@msn.com>
2021-10-12 14:27:07 +0200

    Update tracer.hpp

--- tracer.hpp
+++ tracer.hpp
@@ -3,20 +3,8 @@
 #ifdef TRACE
 #include <iostream>
 #include <sstream>
-
-template<class Arg>
-inline std::string stringify(const Arg& arg) {
-    std::stringstream ss;
-    ss << arg;
-    const auto s = ss.str();
-    return std::is_same<Arg,std::string>::value ? "\""+s+"\"": s; }
-
 class tracer_t {
     int trace_depth = 0;
-    inline void pause() { std::cout << std::endl, std::cin.get(); }
-    template<class Arg, class... Args>
-    inline void pause(const Arg& arg, const Args&... args) {
-        std::cout << arg, pause(args...); }
     inline void prefix(int depth, int line, const std::string& fun) {
         std::cout << std::string(depth,' ') << depth << ":LINE " << line << ": " << fun  << "("; }
     inline void suffix() { std::cout << ") "; }
@@ -25,29 +13,32 @@ class tracer_t {
         if (std::cout << stringify(arg), sizeof...(Args) > 0)
             std::cout << ",";
         suffix(args...); }
+    inline void pause() { std::cout << std::endl, std::cin.get(); }
 public:
+    template<class Arg>
+    inline std::string stringify(const Arg& arg) {
+        std::stringstream ss;
+        ss << arg;
+        const auto s = ss.str();
+        return std::is_same<Arg,std::string>::value ? "\""+s+"\"": s; }
     template<class... Args>
     inline void begin(int line, const std::string& fun, const Args&... args) {
-        prefix(++trace_depth,line,fun), suffix(args...), pause("called"); }
+        prefix(++trace_depth,line,fun), suffix(args...), std::cout << "called", pause(); }
     template<class... Args>
     inline void trace(int line, const std::string& fun, const Args&... args) {
         prefix(trace_depth,line,fun), suffix(args...), pause(); }
     template<class Ans, class... Args>
     inline Ans end(int line, const std::string& fun, const Ans& ans, const Args&... args) {
-        prefix(trace_depth--,line,fun), suffix(args...), pause("return value = ",ans);
+        prefix(trace_depth--,line,fun), suffix(args...), std::cout << "return value = " << ans, pause();
         return ans; } } object;
-
+#define db(arg) std::string(#arg)+" = "+object.stringify(arg)
 #define tr_begin(...) object.begin(__LINE__,__FUNCTION__,__VA_ARGS__)
 #define tr(...)       object.trace(__LINE__,__FUNCTION__,__VA_ARGS__)
 #define tr_end(...)   object.end(__LINE__,__FUNCTION__,__VA_ARGS__)
-#define db(arg) std::string(#arg)+" = "+stringify(arg)
-
 #else
-
 #define tr_begin(...)
 #define tr(...)
 #define tr_end(ans,...) ans
 #define db(arg) arg
-
 #endif // TRACE
 #endif // TRACER_HPP_INCLUDED
